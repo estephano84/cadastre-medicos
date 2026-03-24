241eced010748b5fec4b758ace4faa5daf69cea8
Estephano Akihito Ishii <estephano.i@sempreceub.com>
2026-03-24 11:16:38 -0300

    configurado static

--- cadastre_medicos/settings.py
+++ cadastre_medicos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,6 +74,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_URL = 'login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
