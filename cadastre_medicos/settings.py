b2ac2d0efbb5faf554ece9a2a6c9bcf17079d2fc
Estephano Akihito Ishii <estephano.i@sempreceub.com>
2026-03-24 19:27:00 -0300

    corrigindo css e rotas

--- cadastre_medicos/settings.py
+++ cadastre_medicos/settings.py
@@ -73,8 +73,12 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGIN_URL = 'login/'
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR,'static'),
+]
+
+LOGIN_URL = '/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
