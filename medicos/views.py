062413f6eb9f27e276547977b59ad1c33ff2ee17
Estephano Akihito Ishii <estephano.i@sempreceub.com>
2026-03-26 00:49:01 -0300

    arrumado views

--- medicos/views.py
+++ medicos/views.py
@@ -35,7 +35,7 @@ def editar(request,id):
         medico.telefone = request.POST.get('telefone')
         medico.email = request.POST.get('email')
         medico.save()
-            return redirect('/listar/')
+        return redirect('/listar/')
     return render(request,'medicos/editar.html',{'medico':medico})
 
 def excluir(request, id):
