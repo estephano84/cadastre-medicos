20e1926fd7600c1d9f012965268bd01ad7737a17
Estephano Akihito Ishii <estephano.i@sempreceub.com>
2026-03-26 22:12:43 -0300

    fix urls editar excluir

--- medicos/urls.py
+++ medicos/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [ 
     path('',views.home,name='home'),
-    path('cadastrar/', views.cadastrar_medico, name='cadastrar'),
-    path('listar/', views.listar, name='listar'),
-    path('editar/<int:id/', views.editar, name='editar'),
-    path('excluir/<int:id>/', views.excluir_medico, name='excluir'),
-    path('login/', views.login_usuario, name='login'),
-    path('logout/', views.logout_usuario, name='logout'),
+    path('cadastrar/', views.cadastrar_medico,name='cadastrar'),
+    path('listar/',views.listar,name='listar'),
+    path('editar/<int:id/',views.editar,name='editar'),
+    path('excluir/<int:id>/',views.excluir,name='excluir'),
+    path('login/',views.login_usuario,name='login'),
+    path('logout/',views.logout_usuario,name='logout'),
 ]
