620170b4f335c92da5860017bddf48f64bc1ba71
SeuNome <seuemail@gmail.com>
2026-03-30 00:31:58 -0300

    settings corrigido

--- cadastre_medicos/settings.py
+++ cadastre_medicos/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-troque-isso-depois'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cadastre-medicos.onrender.com','127.0.0.1','localhost']
 
